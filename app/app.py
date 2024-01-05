f83d66b7d8f1f44ba072bbf2f40c1814c060b7dc
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2024-01-05 11:55:39 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -19,4 +19,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
 	count = get_hit_count()
-	return 'Holaaaaaaaa! Este sitio se ha visitado {} veces.\n'.format(count)
+	return 'Hola, nagore! Este sitio se ha visitado {} veces.\n'.format(count)
